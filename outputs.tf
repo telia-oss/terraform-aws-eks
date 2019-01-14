624c3163067d895930e029fde95861fdddec2eff
Tibor Vukovic <tiborvukovic91@gmail.com>
2019-01-14 15:50:23 +0100

    Add arn values to output

--- outputs.tf
+++ outputs.tf
@@ -21,18 +21,34 @@ output "eks_platform_version" {
   value = "${aws_eks_cluster.eks-master.platform_version}"
 }
 
-output "eks_worker_iam_role" {
+output "eks_worker_iam_role_id" {
   value = "${aws_iam_role.eks-worker.id}"
 }
 
-output "eks_worker_security_group" {
+output "eks_worker_iam_role_arn" {
+  value = "${aws_iam_role.eks-worker.arn}"
+}
+
+output "eks_worker_security_group_id" {
   value = "${aws_security_group.eks-worker.id}"
 }
 
-output "eks_master_iam_role" {
+output "eks_worker_security_group_arn" {
+  value = "${aws_security_group.eks-worker.arn}"
+}
+
+output "eks_master_iam_role_id" {
   value = "${aws_iam_role.eks-master.id}"
 }
 
-output "eks_master_security_group" {
+output "eks_master_iam_role_arn" {
+  value = "${aws_iam_role.eks-master.arn}"
+}
+
+output "eks_master_security_group_id" {
   value = "${aws_security_group.eks-master.id}"
+}
+
+output "eks_master_security_group_arn" {
+  value = "${aws_security_group.eks-master.arn}"
 }
