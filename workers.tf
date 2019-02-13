28e1b7de6b3bd36da357ebe6ebef64d378b48c47
Tibor Vukovic <tiborvukovic91@gmail.com>
2019-02-13 15:21:58 +0100

    Allow worker nodes to connect to each other on all ports

--- workers.tf
+++ workers.tf
@@ -61,7 +61,7 @@ resource "aws_security_group" "eks-worker" {
 
 resource "aws_security_group_rule" "eks-worker-ingress-self" {
   description              = "Allow node to communicate with each other"
-  from_port                = 1025
+  from_port                = 0
   protocol                 = "-1"
   security_group_id        = "${aws_security_group.eks-worker.id}"
   source_security_group_id = "${aws_security_group.eks-worker.id}"
